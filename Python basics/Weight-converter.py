57535417880efddae946ac348a65cbcc822fe3e2
Justin-Martindale <130520628+Justin-Martindale@users.noreply.github.com>
2023-04-14 23:33:46 -0500

    new files

---
+++ Python basics/Weight-converter.py
@@ -0,0 +1,14 @@
+weight = float(input("Weight: "))
+unit = input("(L)bs or (K)g: ")
+unit = str(unit.upper())
+
+if unit == "L":
+    converted_weight = weight * 0.45359237
+    converted_unit = "Kg"
+elif unit == "K":
+    converted_weight = weight / 0.45359237
+    converted_unit = "Lbs"
+else:
+    print("Please enter a valid unit")
+
+print("You are " + str(converted_weight) + converted_unit)
